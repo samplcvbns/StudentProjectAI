bfd8000a91448a8efa86852ce9a324a56a3a482a
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2024-12-01 00:39:34 +0100

    updated

--- ai/app/main.py
+++ ai/app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/ai', methods=['GET'])
 def ai():
-    return jsonify({"message": "Hello from AI Service!"})
+    return jsonify({"message": "Hello from Service!"})
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=8000)
--- api/src/server.js
+++ api/src/server.js
@@ -1,5 +1,11 @@
 import express from "express";
 import axios from "axios";
+import fs from "fs";
+import path from "path";
+
+const nodemonConfigPath = path.resolve("nodemon.json");
+const nodemonConfig = JSON.parse(fs.readFileSync(nodemonConfigPath, "utf-8"));
+
 
 const app = express();
 const PORT = 5000;
@@ -8,7 +14,7 @@ const AI_SERVICE_URL = process.env.AI_SERVICE_URL || "http://localhost:8000";
 app.get("/", async (req, res) => {
   try {
     const aiResponse = await axios.get(`${AI_SERVICE_URL}/ai`);
-    res.json({ message: "Hello from Node API Smile!", aiResponse: aiResponse.data });
+    res.json({ message: "Hello from Node AI!", aiResponse: aiResponse.data });
   } catch (error) {
     res.status(500).json({ error: "Failed to connect to AI service" });
   }
