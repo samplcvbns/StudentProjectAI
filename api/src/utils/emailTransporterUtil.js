e05736e296efea18fb7cdf5b063f346f141b3e99
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2024-12-01 19:15:53 +0100

    updated

--- api/src/utils/emailTransporterUtil.js
+++ api/src/utils/emailTransporterUtil.js
@@ -4,6 +4,6 @@ export const transporter = nodemailer.createTransport({
   service: "Gmail", // You can use other services like Outlook, Yahoo, etc.
   auth: {
     user: "chinedusimeon2020@gmail.com", // Your Gmail address
-    pass: "miseopnfheyyjapz", // Your email password or app password
+    pass: "vldfrvswvigkhhci", // Your email password or app password
   },
 });
