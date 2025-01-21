710d87305f68b35dca426d20229963bc818956bd
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2025-01-21 10:06:01 +0100

    used file route

--- api/src/server.js
+++ api/src/server.js
@@ -4,21 +4,20 @@ import fs from "fs";
 import path from "path";
 import mongoose from "mongoose";
 import authRoute from "./routes/authRoute.js";
+import filesRoute from "./routes/filesRoute.js";
 import User from "./models/userModel.js";
 import dotenv from "dotenv";
 dotenv.config();
 
-
 const nodemonConfigPath = path.resolve("nodemon.json");
 const nodemonConfig = JSON.parse(fs.readFileSync(nodemonConfigPath, "utf-8"));
 
-
 const app = express();
 const PORT = 5000;
- const AI_SERVICE_URL = process.env.AI_SERVICE_URL;
+const AI_SERVICE_URL = process.env.AI_SERVICE_URL;
 
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }));
 const connection = mongoose.connect(process.env.MONGODB_URI);
 
 connection.then(() => {
@@ -29,8 +28,9 @@ connection.catch((err) => {
   console.log(err);
 });
 
-
 app.use("/api/auth", authRoute);
+app.use("/api/files", filesRoute);
+
 app.get("/", async (req, res) => {
   try {
     const aiResponse = await axios.get(`${AI_SERVICE_URL}/ai`);
