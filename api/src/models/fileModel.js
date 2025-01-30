42525c784d70f0d48fabf4b0d96775730c58bff0
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2025-01-30 11:39:59 +0100

    added the user id to the schema

--- api/src/models/fileModel.js
+++ api/src/models/fileModel.js
@@ -3,6 +3,11 @@ import mongoose from "mongoose";
 const fileSchema = new mongoose.Schema({
   filename: String,
   url: String,
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
 const File = mongoose.model("File", fileSchema);
