f96b1ce0a16c4bc04393b12fb268bb91095d9ada
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2025-01-30 16:04:06 +0100

    updated

--- api/src/routes/filesRoute.js
+++ api/src/routes/filesRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
-import {uploadFile } from "../controllers/filesController.js";
+import { getUserFile, getUserFiles, uploadFile, downloadFile } from "../controllers/filesController.js";
 import upload from "../middlewares/filesMiddleware.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/upload-file", upload.single("file"), uploadFile);
-
+router.post("/upload-file", verifyToken, upload.single("file"), uploadFile);
+router.get("/my-files", verifyToken, getUserFiles);
+router.get("/my-files/:fileId", verifyToken, getUserFile);
+router.get("/download-file/:id", verifyToken, downloadFile);
 export default router;
