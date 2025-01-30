30c091b28a2f1073977c7d4377f8fd00acab07a0
Nwachukwuchinedu <chinedu.simeon2020@gmail.com>
2025-01-30 16:03:48 +0100

    added iv to schema to decrypt the file

--- api/src/models/fileModel.js
+++ api/src/models/fileModel.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const fileSchema = new mongoose.Schema({
   filename: String,
   url: String,
+  iv: String,
   user_id: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
